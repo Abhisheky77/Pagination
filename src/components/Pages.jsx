ce608b07cae5eedd4e82890da1a524f3cc061980
Abhisheky77 <123infinity890@gmail.com>
2025-12-25 19:29:48 +0530

    Pagination

---
+++ src/components/Pages.jsx
@@ -0,0 +1,128 @@
+import { useEffect, useState } from "react"
+import { MdStars } from "react-icons/md";
+import { Button } from "./ui/button";
+import { HiCurrencyDollar } from "react-icons/hi";
+import { PiCurrencyDollarSimpleFill } from "react-icons/pi";
+import SearchBox from "./SearchBox";
+function Pages() {
+
+    const [loading,setLoading] = useState(false)
+    const [productsData, setProductsData] = useState([])
+    const [filterProCard,setFilterProCard] = useState([])
+    const [count,setCount] = useState(0)
+   const [currentPage,setCurrentPage]= useState(1)
+   const pageSize = 20;
+    console.log("filterProCard",filterProCard);
+
+    async function fetchData() {
+        try {
+            setLoading(true);
+            const response = await fetch(`https://dummyjson.com/products?limit=200&skip=0`)
+            const jsonData = await response.json();
+                
+            setProductsData(jsonData.products)
+            setFilterProCard(jsonData.products)
+            setLoading(false);
+        }
+        catch (e) {
+            console.log(e);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    function handleFilterprouct(data){ 
+        setFilterProCard(data)
+    }
+
+   if (loading) {
+        return (
+            <div className="text-red-500 text-3xl  font-semibold flex justify-center items-center h-screen">
+                Loading data! Please wait...
+            </div>
+        );
+    }
+
+   const totalPages = Math.ceil(filterProCard.length /pageSize );
+   const startIndex = (currentPage - 1) * pageSize ;
+   const endIndex = startIndex + pageSize;
+   const currentProducts = filterProCard.slice(startIndex,endIndex);
+
+    
+    return (
+        <div>
+            <SearchBox  products={productsData} onSearch={handleFilterprouct}/>
+            
+            <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-10 ">
+                {
+                    currentProducts && currentProducts.length ?
+                        currentProducts.map((productItem) =>
+                            <div key={productItem.id} className=" w-80 h-107 rounded-3xl shadow-xl bg-white">
+
+                                <div className=" relative">
+                                    <img src={productItem.images[0]}
+                                        className="  w-full h-70 object-contain rounded-t-3xl bg-gray-200  " />
+                                    <p className={`absolute bottom-0 left-1/2 -translate-x-1/2 px-4 py-1  text-sm font-semibold bg-white/90 rounded-t-md  backdrop-blur  
+                                       ${productItem.availabilityStatus === "In Stock"
+                                            ? " text-green-700 "
+                                            : " text-red-700 "}`}>
+                                        {productItem.availabilityStatus}
+                                    </p>
+                                    { productItem.brand && (<p className=" absolute top-0 left-1/2 -translate-x-1/2 px-4 py-1  text-sm font-semibold   bg-white/70 backdrop-blur-md rounded-b-md shadow-sm ">{productItem.brand}</p>)}
+                                </div>
+
+                                <div className=" p-5">
+                                    <div className=" flex justify-between ">
+                                        <span className="">{productItem.category} </span>
+                                        <span className=" flex items-center " > <MdStars className=" text-yellow-500 text-xl mr-1" />{productItem.rating}</span>
+                                    </div>
+
+                                    <p className=" py-1 font-semibold ">{productItem.title}</p>
+
+                                    <div className=" flex justify-between">
+                                        <span className=" flex items-center font-bold"><HiCurrencyDollar className=" text-yellow-500 text-xl mr-1 " />{productItem.price}</span>
+                                        <Button className="w-40 h-10 rounded-xl bg-gray-600 hover:bg-gray-700 text-white shadow-lg cursor-pointer">Buy</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                        : <div className=" text-3xl font-semibold text-red-500 ">Product are not founds</div>
+                }
+            </div>
+            <div className=" flex justify-center items-center mb-10 gap-2">
+                <Button
+                disabled={currentPage === 1}
+                onClick={()=>setCurrentPage (currentPage -1)}
+                className=" bg-gray-300 text-black hover:bg-gray-400 disabled:opacity-50 text-md"
+
+                >Prev</Button>
+
+                {
+                    [...Array(totalPages)].map((_,index)=>(
+                        <Button
+                        onClick={( )=> setCurrentPage(index +1)}
+                       className={`px-4 py-4 text-md rounded-lg border font-semibold text-black
+                        ${currentPage === index + 1
+                            ? "bg-gray-700 text-white"
+                            : "bg-white hover:bg-gray-200"
+                        }`}
+
+                        >{index+1}</Button>
+                    ))
+                }
+                <Button
+                disabled={currentPage == totalPages}
+                onClick={()=>setCurrentPage(currentPage + 1 )}
+                className="bg-gray-300 text-black text-md hover:bg-gray-400 disabled:opacity-50"
+
+                >Next</Button>
+            </div>
+           
+        </div>
+        
+    )
+}
+export default Pages
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { Button } from "./ui/button";
+
+function SearchBox({products,onSearch}){
+    const [searchText,setSearchText] = useState("");
+    console.log(searchText);
+    console.log(products,"products");
+    
+   function searchProducts(){ 
+    const filterProducts = products.filter((item)=>item.tags.some((tag)=>tag.toLowerCase().includes(searchText.toLowerCase())));
+    onSearch(filterProducts)
+   }
+
+    function handleSearch(){
+        searchProducts()
+        setSearchText("")
+    }
+
+    function handleRefresh(){
+        window.location.reload();
+    }
+    
+    return (
+        <div className=" flex justify-center gap-3 mt-10 sticky top-10 z-10">
+            <input type="text" placeholder="Search products..." value={searchText} onChange={(e)=>setSearchText(e.target.value)}
+            className=" w-70 h-10  rounded-md p-4 focus:outline-none focus:ring-2 focus:ring-gray-400 bg-white border-2 border-gray-300"/>
+             <Button onClick={handleSearch} className=" w-30 h-10">Search</Button>
+             <Button onClick={handleRefresh} className=" w-30 h-10 bg-red-500 hover:bg-red-600">Refresh</Button>
+        </div>
+    )
+}
+export default SearchBox
