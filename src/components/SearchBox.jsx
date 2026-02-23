3fe7c907cbbf7b7c5fbc2f1dee2363ae231c4764
Abhisheky77 <123infinity890@gmail.com>
2026-02-23 22:08:36 +0530

    Pagination

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,6 +5,7 @@ function SearchBox({ products, onSearch }) {
     const [searchText, setSearchText] = useState("");
     const [scrollValue, setScrollValue] = useState(0);
 
+console.log(scrollValue);
 
     function searchProducts() {
         const filterProducts = products.filter((item) =>
